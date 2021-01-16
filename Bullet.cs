714f35ffe8eedba361342db9dbb9f59844919797
yerkko <48332955+yerkko@users.noreply.github.com>
2021-01-16 01:48:08 -0300

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,112 @@
+﻿using BulletHell.Character;
+using UnityEngine;
+
+namespace BulletHell.Spawners
+{
+    public class Bullet : MonoBehaviour
+    {
+        private Vector3 _dirVector = Vector3.zero;
+        // Start is called before the first frame update
+
+        private bool _hasCollided = false;
+
+        private Transform _transform;
+        public  float     m_Acceleration;
+        
+        public float m_Curve;
+
+        public float m_Damage;
+
+        public float m_Direction;
+
+        public float m_DirX;
+
+        public float m_DirY;
+
+        public float m_Speed;
+
+        public float m_Ttl, m_Timer;
+
+
+        public void Launch(float x, float z, float direction, float speed, float acceleration, float curve, float ttl)
+        {
+            var transform1        = transform;
+            var transformPosition = transform1.position;
+            transformPosition.x = x;
+            transformPosition.y = z;
+            transform1.position = transformPosition;
+            m_Direction         = direction;
+            m_Speed             = speed;
+            m_Acceleration      = acceleration;
+            m_Curve             = curve;
+            m_Ttl               = ttl;
+            m_Timer             = 0f;
+        }
+
+        private void Start() { }
+
+        protected virtual void OnCollisionEnter(Collision other)
+        {
+            Hit(other.collider);
+            DestroyBullet();
+        }
+
+
+        protected virtual void Hit(Collider other)
+        {
+            var otherComponent = other.GetComponent<CharacterCombatComponent>();
+            if (otherComponent) DoDamage(otherComponent, m_Color, m_Damage);
+        }
+
+        protected virtual void DestroyBullet()
+        {
+            PoolManager.ReleaseObject(gameObject);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            RunProjectile();
+
+            // check if outside of screen
+
+            // destroy after timeout
+        }
+
+
+        private void RunProjectile()
+        {
+            if (m_Timer >= m_Ttl) DestroyBullet();
+
+            m_Direction += m_Curve        * Time.deltaTime;
+            m_Speed     += m_Acceleration * Time.deltaTime;
+
+            m_DirX = XDir(m_Direction);
+            m_DirY = YDir(m_Direction);
+
+            _dirVector.x = m_DirX;
+            _dirVector.y = m_DirY;
+
+            var transform1 = transform;
+            transform1.position += _dirVector * (m_Speed * Time.deltaTime);
+            m_Timer             += Time.deltaTime;
+        }
+
+        public void OnMove()
+        {
+            ;
+        }
+
+        private static float XDir(float angle)
+        {
+            var rad = Mathf.PI * angle / 180f;
+            return Mathf.Cos(rad);
+        }
+
+        private static float YDir(float angle)
+        {
+            var rad = Mathf.PI * angle / 180f;
+            return -1f * Mathf.Sin(rad);
+        }
+    }
+}
---
+++ BulletSpawner.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+
+namespace BulletHell.Spawners
+{
+    public class BulletSpawner : MonoBehaviour
+    {
+        [Header("Bullet movement parameters")] [SerializeField]
+        public float m_BulletAcceleration;
+
+        [SerializeField] public float m_BulletCurve;
+        [SerializeField] public float m_BulletSpeed      = 1f;
+        [SerializeField] public int   m_BulletsPerArrays = 10;
+        [SerializeField] public float m_BulletTtl        = 5f;
+        [SerializeField] public float m_DefaultAngle;
+
+        [SerializeField] public float m_FireRate = 5f;
+
+        [Header("Spin parameters")] [SerializeField]
+        public float m_SpinRate;
+
+        [SerializeField] public float m_MaxSpinRate;
+        [SerializeField] public float m_SpinModificator;
+        [SerializeField] public bool  m_InvertSpin;
+
+        [Header("Spawner size")] [SerializeField]
+        public float m_ObjectHeight;
+
+        [SerializeField] public float m_ObjectWidth;
+
+
+        [SerializeField] public int         m_PatternArrays = 2;
+        private                 GameObject  _bulletRoot;
+        private                 float       _shoot;
+        private                 bool        _shouldShoot;
+        public                  Vector3     m_SpawnPosition = Vector3.zero;
+
+
+        [Header("Spawn parameters")] [SerializeField]
+        public float m_SpreadBetweenArray = 180f;
+
+        [SerializeField] public float m_SpreadWithinArray = 90f;
+        [SerializeField] public float m_StartAngle;
+
+
+        [SerializeField] public float m_BeginSpinSpeed = 1f;
+
+        [SerializeField] public GameObject m_BulletPrefab;
+
+
+        public void Awake() { }
+
+
+        public void Start()
+        {
+            PoolManager.Instance.warmPool(m_BulletPrefab, 100);
+        }
+
+        public void Update()
+        {
+            Draw();
+        }
+
+        public void Draw()
+        {
+            CalculateBullets();
+
+            m_DefaultAngle %= 360f;
+            m_DefaultAngle += m_SpinRate        * Time.deltaTime;
+            m_SpinRate     += m_SpinModificator * Time.deltaTime;
+
+            if (m_InvertSpin && (m_SpinRate < -m_MaxSpinRate || m_SpinRate > m_MaxSpinRate))
+                m_SpinModificator = -m_SpinModificator;
+
+            _shoot += Time.deltaTime;
+
+            if (_shoot >= m_FireRate)
+            {
+                _shouldShoot = true;
+                _shoot       = 0;
+            }
+            else
+            {
+                _shouldShoot = false;
+            }
+        }
+
+        public void CalculateBullets()
+        {
+            var bulletLength                    = m_BulletsPerArrays - 1;
+            if (bulletLength == 0) bulletLength = 1;
+
+            var arrayLength = m_PatternArrays - m_PatternArrays;
+
+            if (arrayLength == 0) arrayLength = 1;
+
+            var arrayAngle  = m_SpreadWithinArray  / bulletLength;
+            var bulletAngle = m_SpreadBetweenArray / arrayLength;
+
+            if (!_shouldShoot) return;
+            for (var i = 0; i < m_PatternArrays; i++)
+            for (var j = 0; j < m_BulletsPerArrays; j++)
+                CalculateBullet(i, j, arrayAngle, bulletAngle);
+        }
+
+
+        public void CalculateBullet(int i, int j, float arrayAngle, float bulletAngle)
+        {
+            var position = transform.position;
+            var x1 = position.x + LengthDirX(m_ObjectWidth,
+                                             m_DefaultAngle + bulletAngle * i + arrayAngle * j + m_StartAngle);
+            var y1 = position.y + LengthDirZ(m_ObjectHeight,
+                                             m_DefaultAngle + bulletAngle * i + arrayAngle * j + m_StartAngle);
+
+            var direction = m_DefaultAngle + bulletAngle * i + arrayAngle * j + m_StartAngle;
+
+            m_SpawnPosition.x = x1;
+            m_SpawnPosition.y = y1;
+            m_SpawnPosition.y = position.y;
+            var bullet           = FireBullet(m_SpawnPosition, Quaternion.identity);
+            var bullet_component = bullet.GetComponent<Bullet>();
+            bullet_component.Launch(x1, y1, direction, m_BulletSpeed, m_BulletAcceleration, m_BulletCurve, m_BulletTtl);
+            //_bulletList.Add(bullet_component);
+        }
+
+
+        public GameObject FireBullet(Vector3 position, Quaternion rotation)
+        {
+            return PoolManager.SpawnObject(m_BulletPrefab, position, rotation);
+        }
+
+
+        private static float LengthDirX(float distance, float angle)
+        {
+            return distance * Mathf.Cos(Mathf.PI * angle / 180f);
+        }
+
+        private static float LengthDirZ(float distance, float angle)
+        {
+            return distance * Mathf.Sin(Mathf.PI * angle / 180f);
+        }
+    }
+}
